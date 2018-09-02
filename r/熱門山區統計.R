5026bdbc61fcc341230d8d474f8d850dd1b4168b
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2018-09-02 14:47:51 +0800

    修正條表標題。

--- r/熱門山區統計.R
+++ r/熱門山區統計.R
@@ -17,8 +17,6 @@ location_db <- tbl(con, "location")
 library('ggthemr')
 ggthemr('grass')
 
-
-
 # 熱門地點
 location_db  %>%
   filter(year == 2018) %>% 
@@ -28,13 +26,12 @@ location_db  %>%
   filter(n > 1500) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2018 年 8 月以前山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2018 年 8 月以前山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2018 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
-
+ggsave("201808/chart/2018 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 location_db  %>%
   filter(year == 2017) %>% 
@@ -44,12 +41,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2017 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2017 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2017 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2017 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # 2016
 location_db  %>%
@@ -60,12 +57,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2016 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2016 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2016 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2016 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # 2015
 location_db  %>%
@@ -76,12 +73,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2015 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2015 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2015 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2015 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # 2014
 location_db  %>%
@@ -92,12 +89,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2014 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2014 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2014 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2014 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # 2013
 location_db  %>%
@@ -108,12 +105,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2013 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2013 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2013 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2013 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # 2012
 location_db  %>%
@@ -124,12 +121,12 @@ location_db  %>%
   filter(n > 3000) %>%
   ggplot() + 
   geom_col(aes(x = mountain, y=n), show.legend = TRUE) +
-  labs(title = "2012 年山地管制區超過三千申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
+  labs(title = "2012 年山地管制區超過三千次申請之熱門山區", caption ="資料來源內政部警政署 20180901") +
   xlab("山區") +
   ylab("申請次數") +
   geom_text(stat='identity', aes(label=n, x=mountain, y=n), vjust  = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("201808/chart/2012 年山地管制區超過三千申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
+ggsave("201808/chart/2012 年山地管制區超過三千次申請之熱門山區.png", plot = last_plot(), device = "png", dpi = 600)
 
 # filter(mountain != '慕谷慕魚') %>%
 # filter(mountain != '清水溪上游') %>%
